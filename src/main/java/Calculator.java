ef17e7fda211a69b6fff7f3799926e000bc3cdf4
nerikeb <nerike.b@gmail.com>
2025-10-26 09:45:13 +0100

    Added division method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,4 +11,8 @@ public class Calculator {
     public int multiplication(int a, int b) {
         return a * b;
     }
+
+    public double division(int a, int b) {
+        return (double) a / b;
+    }
 }
